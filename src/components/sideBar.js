43312d338f2f203392f6f2f83ab046dd9ddc1d53
Luis Alberto Gudiño Salinas <elios264@hotmail.com>
2017-12-16 18:23:24 -0600

    + sidebar

--- src/components/sideBar.js
+++ src/components/sideBar.js
@@ -1,7 +1,24 @@
 import PropTypes from 'prop-types';
 import React, { PureComponent } from 'react';
-import { Text, View } from 'react-native';
-import NativeTachyons from 'react-native-style-tachyons';
+import { Text, View, Image, TouchableOpacity } from 'react-native';
+import NativeTachyons, { sizes } from 'react-native-style-tachyons';
+import _ from 'lodash';
+
+const MenuElement = NativeTachyons.wrap(({ image, text }) => (
+  <TouchableOpacity onPress={_.noop}>
+    <View cls='h3 b--#171717 bt flx-row'>
+      <View cls='w3 aife jcc'>
+        <Image cls='w2 h2 rm-contain' source={image} />
+      </View>
+      <Text cls='white ff-ubu-b f5 ml3 asc'>{text}</Text>
+    </View>
+  </TouchableOpacity>
+));
+MenuElement.propTypes = {
+  image: PropTypes.number.isRequired,
+  text: PropTypes.string.isRequired,
+};
+
 
 @NativeTachyons.wrap
 export class Sidebar extends PureComponent {
@@ -10,14 +27,34 @@ export class Sidebar extends PureComponent {
     drawer: PropTypes.object.isRequired,
   }
 
+  closeDrawer = () => {
+    this.props.drawer.closeDrawer();
+  }
+
   render() {
-    //const { drawer } = this.props;
 
     return (
-      <View cls='flx-i jcc aic bg-primarydark'>
-        <Text cls='contrast f4 tc white'>
-          Menu here on own file!
-        </Text>
+      <View cls='flx-i bg-primarydark'>
+        <View cls='bg-primary aic jcc pv4'>
+          <TouchableOpacity cls='absolute left-1' onPress={this.closeDrawer}>
+            <Image cls='h1 w1' source={require('fc_juarez/assets/img/back.png')} />
+          </TouchableOpacity>
+          <Text cls='white f3 ff-permanent-marker'> FC Juarez <Text cls='contrast'>App </Text></Text>
+          <Text cls='gray ff-ubu-b absolute' style={{ fontSize: sizes.f5 / 2, bottom: sizes.mb1 + sizes.mb3 }}>
+            FC JÚAREZ | V1.0
+          </Text>
+        </View>
+        <View cls='flx-i'>
+          <MenuElement image={require('fc_juarez/assets/img/menu/ball.png')} text='Próximo partido' />
+          <MenuElement image={require('fc_juarez/assets/img/menu/score.png')} text='Tabla general' />
+          <MenuElement image={require('fc_juarez/assets/img/menu/tv.png')} text='Calendario de partidos' />
+          <MenuElement image={require('fc_juarez/assets/img/menu/whistle.png')} text='Minuto a minuto' />
+          <MenuElement image={require('fc_juarez/assets/img/menu/timer.png')} text='Configuración' />
+
+          <Text cls='gray ff-ubu-b absolute bottom-1 left-1' style={{ fontSize: sizes.f5 / 2 }}>
+            Todos los derechos reservados 2017
+          </Text>
+        </View>
       </View>
     );
   }
