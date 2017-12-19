5dbc0bef98a742bd64f6c5c1f64f36f0b085f866
Luis Alberto Gudiño Salinas <elios264@hotmail.com>
2017-12-19 12:55:27 -0600

    + standings

--- src/components/standings.js
+++ src/components/standings.js
@@ -1,7 +1,19 @@
 import React, { PureComponent } from 'react';
 import { StyleSheet, View, Dimensions, Image, ScrollView, Text } from 'react-native';
-import NativeTachyons from 'react-native-style-tachyons';
+import NativeTachyons, { sizes } from 'react-native-style-tachyons';
 import ScalableImage from 'react-native-scalable-image';
+
+const TeamInfo = NativeTachyons.wrap(({ image, name }) => ( // eslint-disable-line react/prop-types
+  <View cls='flx-row aic mv1 mr2 h3'>
+    <Image source={image} cls='rm-contain' style={[styles.logoSize]} />
+    <Text cls='white ff-ubu-b f6 ml2 tc flx-i'>{name}</Text>
+  </View>
+));
+const Score = NativeTachyons.wrap(({ score }) => ( // eslint-disable-line react/prop-types
+  <View cls='h3 jcc aife ml1 mr2 mv1'>
+    <Text cls='f6 ff-ubu-b white'>{score}</Text>
+  </View>
+));
 
 @NativeTachyons.wrap
 export class Standings extends PureComponent {
@@ -13,7 +25,119 @@ export class Standings extends PureComponent {
         <View cls='flx-i bg-primary'>
           <Image cls='absolute-fill rm-cover' style={[styles.expand]} source={require('fc_juarez/assets/img/background.png')} />
           <ScrollView cls='flx-i'>
-            <Text cls='f1 white'>Tabla general</Text>
+            <Text cls='mv3 ml3 f3 ff-ubu-m white'>Tabla <Text cls='#AAAAAA'>general</Text></Text>
+            <View cls='bt b--#373737' />
+            <View cls='mt3 ml2 mr1 flx-row'>
+              <View cls='flx-i' style={[styles.maxDesc]}>
+                <View cls='h2 jcc'>
+                  <Text cls='white ff-ubu'>Liga de ascenso</Text>
+                </View>
+                <TeamInfo image={require('fc_juarez/assets/img/teams/celayafc.png')} name='Celaya F.C.' />
+                <TeamInfo image={require('fc_juarez/assets/img/teams/fcjuarez.png')} name='FC Juárez' />
+                <TeamInfo image={require('fc_juarez/assets/img/teams/tmfutbolclub.png')} name='TM Futbol Club' />
+                <TeamInfo image={require('fc_juarez/assets/img/teams/clubatleticozacatepec.png')} name='Atlético Zacatepec' />
+                <TeamInfo image={require('fc_juarez/assets/img/teams/alebrijes.png')} name='Alebrijes' />
+                <TeamInfo image={require('fc_juarez/assets/img/teams/mineros.png')} name='Mineros' />
+                <TeamInfo image={require('fc_juarez/assets/img/teams/cafetaleros.png')} name='Cafetaleros' />
+              </View>
+              <View>
+                <View cls='h2 jcc'>
+                  <Text cls='contrast ff-ubu-b ml1 mr2 f6'>JJ</Text>
+                </View>
+                <Score score={15} />
+                <Score score={15} />
+                <Score score={15} />
+                <Score score={15} />
+                <Score score={15} />
+                <Score score={15} />
+                <Score score={15} />
+              </View>
+              <View>
+                <View cls='h2 jcc'>
+                  <Text cls='contrast ff-ubu-b ml1 mr2 f6'>JG</Text>
+                </View>
+                <Score score={8} />
+                <Score score={8} />
+                <Score score={8} />
+                <Score score={8} />
+                <Score score={8} />
+                <Score score={8} />
+                <Score score={8} />
+              </View>
+              <View>
+                <View cls='h2 jcc'>
+                  <Text cls='contrast ff-ubu-b ml1 mr2 f6'>JE</Text>
+                </View>
+                <Score score={4} />
+                <Score score={4} />
+                <Score score={4} />
+                <Score score={4} />
+                <Score score={4} />
+                <Score score={4} />
+                <Score score={4} />
+              </View>
+              <View>
+                <View cls='h2 jcc'>
+                  <Text cls='contrast ff-ubu-b ml1 mr2 f6'>JP</Text>
+                </View>
+                <Score score={3} />
+                <Score score={3} />
+                <Score score={3} />
+                <Score score={3} />
+                <Score score={3} />
+                <Score score={3} />
+                <Score score={3} />
+              </View>
+              <View>
+                <View cls='h2 jcc'>
+                  <Text cls='contrast ff-ubu-b ml1 mr2 f6'>GF</Text>
+                </View>
+                <Score score={17} />
+                <Score score={17} />
+                <Score score={17} />
+                <Score score={17} />
+                <Score score={17} />
+                <Score score={17} />
+                <Score score={17} />
+              </View>
+              <View>
+                <View cls='h2 jcc'>
+                  <Text cls='contrast ff-ubu-b ml1 mr2 f6'>GC</Text>
+                </View>
+                <Score score={9} />
+                <Score score={9} />
+                <Score score={9} />
+                <Score score={9} />
+                <Score score={9} />
+                <Score score={9} />
+                <Score score={9} />
+              </View>
+              <View>
+                <View cls='h2 jcc'>
+                  <Text cls='contrast ff-ubu-b ml1 mr2 f6'>DIF</Text>
+                </View>
+                <Score score={8} />
+                <Score score={8} />
+                <Score score={8} />
+                <Score score={8} />
+                <Score score={8} />
+                <Score score={8} />
+                <Score score={8} />
+              </View>
+              <View>
+                <View cls='h2 jcc'>
+                  <Text cls='contrast ff-ubu-b ml1 mr2 f6'>PTS</Text>
+                </View>
+                <Score score={28} />
+                <Score score={28} />
+                <Score score={28} />
+                <Score score={28} />
+                <Score score={28} />
+                <Score score={28} />
+                <Score score={128} />
+              </View>
+
+            </View>
           </ScrollView>
         </View>
         <ScalableImage width={Dimensions.get('window').width} source={require('fc_juarez/assets/img/temp/ad.png')} />
@@ -27,4 +151,11 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%'
   },
+  logoSize: {
+    width: sizes.w2 + sizes.w1,
+    height: sizes.h1 + sizes.h2
+  },
+  maxDesc: {
+    maxWidth: sizes.w4 + sizes.w2
+  }
 });
