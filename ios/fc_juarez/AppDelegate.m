06f6dd45a4a8de84cbb405c991463829a7bc2e12
X-Evolvers <Developer@MacBook-Air-de-X-Evolvers.local>
2018-01-09 17:29:02 -0600

    + integrate onesignal in ios

--- ios/fc_juarez/AppDelegate.m
+++ ios/fc_juarez/AppDelegate.m
@@ -13,6 +13,7 @@
 #import <React/RCTRootView.h>
 
 @implementation AppDelegate
+@synthesize oneSignal = _oneSignal;
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
@@ -25,6 +26,9 @@
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
   rootView.backgroundColor = [UIColor blackColor];
+  
+  self.oneSignal = [[RCTOneSignal alloc] initWithLaunchOptions:launchOptions
+                                                         appId:@"5c13208c-4b79-4a14-bf39-bee0a9515b7a"];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
--- src/components/app.js
+++ src/components/app.js
@@ -4,6 +4,7 @@ import { StyleSheet, View, StatusBar } from 'react-native';
 import { connect } from 'react-redux';
 import Drawer from 'react-native-drawer-menu';
 import { Route, Switch } from 'react-router-native';
+import OneSignal from 'react-native-onesignal';
 
 import { Sidebar } from './sideBar';
 import { Header } from './header';
@@ -23,6 +24,39 @@ export class App extends PureComponent {
   static propTypes = {
     initializing: PropTypes.bool.isRequired,
   }
+
+  componentWillMount() {
+    OneSignal.addEventListener('received', this.onReceived);
+    OneSignal.addEventListener('opened', this.onOpened);
+    OneSignal.addEventListener('registered', this.onRegistered);
+    OneSignal.addEventListener('ids', this.onIds);
+  }
+
+  componentWillUnmount() {
+      OneSignal.removeEventListener('received', this.onReceived);
+      OneSignal.removeEventListener('opened', this.onOpened);
+      OneSignal.removeEventListener('registered', this.onRegistered);
+      OneSignal.removeEventListener('ids', this.onIds);
+  }
+
+  onReceived(notification) {
+      console.log("Notification received: ", notification);
+  }
+
+  onOpened(openResult) {
+    console.log('Message: ', openResult.notification.payload.body);
+    console.log('Data: ', openResult.notification.payload.additionalData);
+    console.log('isActive: ', openResult.notification.isAppInFocus);
+    console.log('openResult: ', openResult);
+  }
+
+  onRegistered(notifData) {
+      console.log("Device had been registered for push notifications!", notifData);
+  }
+
+  onIds(device) {
+    console.log('Device info: ', device);
+  }  
 
   setDrawerRef = (ref) => this.drawer = ref;
 
