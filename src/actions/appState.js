1d455beae862339734417fd4782bea4ee95796bd
Luis Alberto Gudiño Salinas <elios264@hotmail.com>
2018-01-26 11:10:20 -0600

    + check for push permissions in every foreground

--- src/actions/appState.js
+++ src/actions/appState.js
@@ -8,13 +8,14 @@ const subscribeToAppStateChanged = (handler) => {
 };
 
 export const appStart = () => (dispatch) => {
-  const appStateListener = (state) => {
-    __DEV__ && console.log({ type: 'APPSTATE_CHANGED', state });
-    dispatch({ type: 'APPSTATE_CHANGED', state });
-  };
   const permissionsHandler = (state) => {
     __DEV__ && console.log({ type: 'PUSH_PERMISSIONS_CHANGED', state });
     dispatch({ type: 'PUSH_PERMISSIONS_CHANGED', state });
+  };
+  const appStateListener = (state) => {
+    __DEV__ && console.log({ type: 'APPSTATE_CHANGED', state });
+    dispatch({ type: 'APPSTATE_CHANGED', state });
+    if (state === 'active') OneSignal.getPermissionSubscriptionState(permissionsHandler);
   };
 
   subscribeToAppStateChanged(appStateListener),
