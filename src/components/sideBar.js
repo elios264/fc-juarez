27ad4af48ba49c5f608184482e5552e364847572
Luis Alberto Gudiño Salinas <elios264@hotmail.com>
2018-02-21 20:02:50 -0600

    + updating logo

--- src/components/sideBar.js
+++ src/components/sideBar.js
@@ -67,14 +67,20 @@ export class Sidebar extends PureComponent {
           <MenuElement path='/the-minute' image={require('fc_juarez/assets/img/menu/whistle.png')} text='Minuto a minuto' />
           <MenuElement path='/settings' image={require('fc_juarez/assets/img/menu/timer.png')} text='Configuración' />
 
-          <View cls='absolute bottom-1 left-1' >
-            <TouchableOpacity onPress={this.openEuphos} >
-              <Image cls='mb1 rm-contain w3' source={require('fc_juarez/assets/img/menu/eupho.png')} />
-            </TouchableOpacity>
-            <Text cls='gray ff-ubu-b' style={{ fontSize: sizes.f5 / 2 }}>
-              Todos los derechos reservados 2018
-            </Text>
+          <View cls='flx-i jcc' >
+            <View cls='flx-row' >
+              <TouchableOpacity onPress={this.openEuphos} >
+                <View cls='aic ml4' >
+                  <Image cls='mb1 rm-contain w4' source={require('fc_juarez/assets/img/menu/eupho.png')} />
+                  <Text cls='white ff-ubu-b f5 asc'>Ve los partidos en vivo</Text>
+                </View>
+              </TouchableOpacity>
+            </View>
           </View>
+
+          <Text cls=' mb3 ml3 gray ff-ubu-b' style={{ fontSize: sizes.f5 / 2 }}>
+              Todos los derechos reservados 2018
+          </Text>
         </View>
       </View>
     );
